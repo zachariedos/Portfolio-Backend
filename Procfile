c1a8c9ecf15a285800fbf4442d21a8560259ae0d
Zacharie <zacharie21000@gmail.com>
2025-02-17 13:05:28 +0100

    feat: intégration heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node --env-file=.env app.js
--- server.js
+++ server.js
@@ -44,4 +44,5 @@ server.on("listening", () => {
   const bind = typeof address === "string" ? "pipe " + address : "port " + port;
   console.log("Listening on " + bind);
 });
-server.listen(port);
+
+server.listen(port,"0.0.0.0");
